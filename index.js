9fa0cb070cad148f40ff48cb403c5875f676d4c8
Daniel Tralamazza <tralamazza@gmail.com>
2012-09-11 17:40:26 +0200

    init

---
+++ index.js
@@ -0,0 +1,61 @@
+var request = require('request');
+var url = require('url');
+
+var Capsule = function(account, key) {
+  var self = this;
+
+  /**
+   * options = {
+   *   method (GET, PUT ...)
+   *   path (/party)
+   *   search (foo=bar)
+   *   data (object to be JSONinified)
+   * }
+   *
+   * cb = function(err, data)
+   */
+  self.request = function(options, cb) {
+    var opt = {
+      url: url.format({
+        protocol: 'https',
+        auth: key + ':x',
+        host: account + '.capsulecrm.com',
+        pathname: '/api' + options.path,
+        search: options.search || ''
+      }),
+      headers: {
+        Accept: 'application/json'
+      },
+      method: options.method || 'GET'
+    };
+    if (options.data)
+      opt.json = JSON.stringify(options.data);
+    request(opt, function(err, res, body) {
+      if (!err && res.statusCode === 200)
+        cb(null, JSON.parse(body));
+      else
+        cb(err);
+    });
+  };
+
+  // create simple listing calls
+  var listers = [
+    'countries',
+    'currencies',
+    'organisation',
+    'person',
+    'party',
+    'opportunity',
+    'kase',
+    'tasks'
+  ];
+  listers.forEach(function(li) {
+    self[li] = function(cb) {
+      self.request({ path: '/' + li }, cb);
+    };
+  });
+};
+
+exports.createConnection = function(account, key) {
+  return new Capsule(account, key);
+};
